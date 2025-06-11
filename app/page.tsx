a0fffbb01c6bda3867b6d79bd7c6e4965d06ed70
vishalBelwal <vishalbelwal52@gmail.com>
2025-06-11 16:54:32 +0530

    added contactUs in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { FeaturedProperties } from "@/components/featured-properties"
 import { WhyChooseUs } from "@/components/why-choose-us"
 import { PropertyStats } from "@/components/property-stats"
 import { TestimonialsSection } from "@/components/testimonials-section"
-import { CTASection } from "@/components/cta-section"
 import { Footer } from "@/components/footer"
+import { ContactUs } from "@/components/ContactUs"
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
       <FeaturedProperties />
       <WhyChooseUs />
       <TestimonialsSection />
-      <CTASection />
+      <ContactUs />
       <Footer />
     </main>
   )
